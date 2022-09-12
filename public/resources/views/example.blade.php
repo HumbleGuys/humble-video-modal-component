343281b59f440ce2b78a516c5d135176e93e149d
Kim Larsson <kim@humbleguys.se>
2022-09-12 13:53:50 +0200

    Better support for backdrop

--- public/resources/views/example.blade.php
+++ public/resources/views/example.blade.php
@@ -1,14 +1,16 @@
 <x-layout>
-    <x-videoModal::base class="myVideoModal">
-        <x-videoModal::closeButton class="myVideoModal__closeButton" />
-
-        <x-videoModal::player class="myVideoModal__player" />
-    </x-videoModal::base>
-
     <x-videoModal::trigger
         video="https://www.youtube.com/watch?v=Mkx3e_HK7N4"
         class="myTriggerButton"
     >
         play video
     </x-videoModal::trigger>
+
+    <x-videoModal::base class="myVideoModal">
+        <x-videoModal::backdrop class="myVideoModal__backdrop" />
+
+        <x-videoModal::closeButton class="myVideoModal__closeButton" />
+
+        <x-videoModal::player class="myVideoModal__player" />
+    </x-videoModal::base>
 </x-layout>
